dcba13f0a517d1a252cbba5216190e862e46326d
ISHIMWE-Bosco <b.ishimwe3@alustudent.com>
2024-11-15 15:27:38 +0200

    gggg

---
+++ 01-fundamentals/01-conditionals/loop_odd_even.js
@@ -0,0 +1,16 @@
+for ( i= 0; i<16; i++) {
+    console.log([i]);
+
+    if (i%2 ===0) {
+        console.log(`${i} is even`);
+     } else if ( i ===0 ) {
+        console.log(`${i} is even`);
+     }
+
+    else {
+    console.log(`${i} is odd`);
+       
+}
+}
+ 
+    
