215b3ac33a0fe9cba22371b8cf65ac271d802f6e
ISHIMWE-Bosco <b.ishimwe3@alustudent.com>
2024-11-15 14:19:35 +0200

    hhfff

--- 01-fundamentals/01-conditionals/maximum.js
+++ 01-fundamentals/01-conditionals/maximum.js
@@ -4,13 +4,13 @@ var c = -6;
 var d = 0;
 var e = 1;
 
-if (a >= b && a >= c && a >= d && a >= e) {
+if (a > b && a > c && a > d && a > e) {
     console.log(`${a} is the bigger number`);
-} else if (b >= a && b >= c && b >= d && b >= e) {
+} else if (b > a && b > c && b > d && b > e) {
     console.log(`${b} is the bigger number`);
-} else if (c >= a && c >= b && c >= d && c >= e) {
+} else if (c > a && c > b && c > d && c > e) {
     console.log(`${c} is the bigger number`);
-} else if (d >= a && d >= b && d >= c && d >= e) {
+} else if (d > a && d > b && d > c && d > e) {
     console.log(`${d} is the bigger number`);
 } else {
     console.log(`${e} is the bigger number`);
