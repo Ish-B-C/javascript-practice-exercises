7236bb85391eb08b80267711a7d04e28f2ca0ea9
ISHIMWE-Bosco <b.ishimwe3@alustudent.com>
2024-11-18 11:08:08 +0200

    clone array

--- 01-fundamentals/02-arrays/clone_array.js
+++ 01-fundamentals/02-arrays/clone_array.js
@@ -1,9 +1,9 @@
-function cloneArray(arr) {
+function clone(arr) {
     return arr.slice();
     
 }
 var originalArray = [1,2,3,4];
-var clonedArray = cloneArray(originalArray);
+var clonedArray = clone(originalArray);
 console.log(clonedArray);
 console.log(clonedArray === originalArray);
 
